2a2a68433c7d9b39278415dc768071f2dba0ac76
Amber De Koster <killboy1100@gmail.com>
2026-06-01 11:29:41 +0200

    starting Login and Registry functionality

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ dotenv.config();
 try {
     const app = express();
 
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        next();
+    });
+
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+
+        if (req.method === "OPTIONS") {
+            return res.sendStatus(204);
+        }
+        next();
+    });
+
+    //Middelware to support application/JSON content-type
+    app.use(express.json());
+    //Middelware to support application/x-www-form-urlencoded content-type
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use("/login", loginRouter);
+
     app.listen(8000, () => console.log('Server running on port 8000'));
 } catch (e) {
     console.log(e);
